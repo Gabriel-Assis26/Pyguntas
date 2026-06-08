633e137d478121b573d35b62712e29447fef4d02
Gabriel-Assis26 <1661506@sga.pucminas.br>
2026-06-08 17:50:25 -0300

    Traduzi as perguntas e respostas

--- src/jogo.py
+++ src/jogo.py
@@ -1,4 +1,5 @@
 import pygame
+from deep_translator import GoogleTranslator
 from src.funcoes import (
     buscar_perguntas,
 )
@@ -15,17 +16,25 @@ def executar_jogo():
     pAtual = 0
     rodando = True
     while rodando:
-        tela.fill((0,0,0)) #tela limpa a cada frame
+        tela.fill((0,0,0))
         fonte = pygame.font.SysFont("Arial", 24)
+        tradPergunta = GoogleTranslator(
+            source='en',
+            target='pt'
+        ).translate(perguntas[pAtual]['question'])
         texto = fonte.render(
-            perguntas[pAtual]['question'],
+            tradPergunta,
             True,
             (255,255,255)
         )
         tela.blit(texto, (100,100))
         botao_correta = pygame.Rect(100, 300, 300, 60)
         pygame.draw.rect(tela, (0, 100, 255), botao_correta)
-        texto = fonte.render(perguntas[pAtual]['correct_answer'], True, (255, 255, 255))
+        tradCerta = GoogleTranslator(
+            source='en',
+            target='pt'
+        ).translate(perguntas[pAtual]['correct_answer'])
+        texto = fonte.render(tradCerta, True, (255, 255, 255))
         tela.blit(texto, texto.get_rect(center=botao_correta.center))
         
         botoes_erradas = []
@@ -36,9 +45,13 @@ def executar_jogo():
                 300,
                 60
             )
+            tradErrada = GoogleTranslator(
+                source='en',
+                target='pt'
+            ).translate(text)
             botoes_erradas.append(botao)
             pygame.draw.rect(tela,(0,100,255),botao)
-            texto = fonte.render(text, True, (255, 255, 255))
+            texto = fonte.render(tradErrada, True, (255, 255, 255))
             texto_rect = texto.get_rect(center=botao.center)
             tela.blit(texto, texto.get_rect(center=botao.center))
         for evento in pygame.event.get():
